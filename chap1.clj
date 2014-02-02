5978bf8f37e3424eae2ffa1385af39b13c514ad4
和気 夏彦 <wake@air.local>
2014-02-02 21:24:50 +0900

    問題1.14まで

--- chap1.clj
+++ chap1.clj
@@ -136,6 +136,69 @@
   (if (= count 0)
     b
     (fib-iter (+ a b) a (dec count))))
-
 (defn fib [n]
   (fib-iter 1 0 n))
+
+;; 両替の計算（木構造再帰なので、実行時間がかかる）
+(defn first-denomination [kinds-of-coins]
+  (cond (= kinds-of-coins 1) 1
+        (= kinds-of-coins 2) 5
+        (= kinds-of-coins 3) 10
+        (= kinds-of-coins 4) 25
+        (= kinds-of-coins 5) 50))
+(defn cc [amount kinds-of-coins]
+  (cond (= amount 0) 1
+        (or (< amount 0) (= kinds-of-coins 0) ) 0
+        :else (+ (cc amount (dec kinds-of-coins))
+                 (cc (- amount (first-denomination kinds-of-coins)) kinds-of-coins))))
+(defn count-change [amount]
+  (cc amount 5))
+(count-change 1000)
+
+
+;;; 問題1.11
+;; 再帰的
+(defn fn-recur [n]
+  (if (< n 3)
+    n
+    (+ (fn-recur (- n 1))
+       (* 2 (fn-recur (- n 2)))
+       (* 3 (fn-recur (- n 3))))))
+
+;; 反復的
+(defn fn-iter [n]
+  (defn iter [a b c count]
+    (cond (= count 0) c
+          (= count 1) b
+          :else (iter (+ a (* 2 b) (* 3 c)) a b (dec count))))
+  (iter 2 1 0 n))
+
+
+;;; 問題1.12
+(defn pascal-tri [m n]
+  (if (or (<= n 1) (>= n m))
+    1
+    (+ (pascal-tri (- m 1) (- n 1))
+       (pascal-tri (- m 1) n))))
+
+
+;;; 問題1.13
+;　うっ
+
+
+;;; 1.2.3増加の程度
+; 関数の増加の程度
+; θ(n)：線形（再帰プロセス）
+; θ(1)：定数（反復プロセス）
+; θ(φ^n)：木構造のステップ
+
+
+;;; 問題1.14
+(defn p [x]
+  (- (* 3 x) (* 4 (cube x))))
+(defn sine [angle]
+  (if (not (> (Math/abs angle) 0.1))
+    angle
+    (p (sine (/ angle 3.0)))))
+; a. 5回
+; b. うっ
